9b7136c6acfd552f51e586b3ad447b69deadf668
aschneid <>
2013-06-03 15:04:32 +0000

    Added comments, made runnable in lxplus, removed unused code fragments.

--- SuperLayerAlignment/mual_plots/python/align_plots.py
+++ SuperLayerAlignment/mual_plots/python/align_plots.py
@@ -16,10 +16,9 @@ class corrections_plotter:
         self.data = []
         self.sign_conventions = {}
         self.canvas = TCanvas()
-        self.hps = []
-        self.frames = []
-        self.framess = []
-        self.file_list = []
+        self.hps = [] # 2d array of TGraphErrors
+        self.frames = [] # 2d array of TH1F
+        self.file_list = [] # file indexes to be used in plotting
         self.title = "Title"
         self.all_plots  = True
         self.plot_range = (0,0,0,0)
@@ -57,26 +56,34 @@ class corrections_plotter:
         self.scm.append(["dai", "signx"])
         self.scm.append(["dphizsl2", "signx", "signy"])
 
+        # Variables to plot and organize grid by
         self.x_var = ""
         self.y_var = ""
         self.xo_var = ""
         self.yo_var = ""
 
+    # Set the range of plots that appear on the grid
     def setPlotRange(self, x_min, y_min, x_max, y_max):
         self.plot_range = (x_min, y_min, x_max, y_max)
         self.all_plots = False
 
+    # Plot all possible plots on grid
     def setPlotAll(self, b = True):
         self.all_plots = b
-
+        
+    # Get the canvas
     def getCanvas(self):
         return self.canvas
 
+    # Create the canvas with specific style options
     def createCanvas(self):
         #create canvas and set style
         self.canvas = TCanvas("c1",self.title,200,10,700,500)
         self.canvas.SetGrid(True)
+        self.canvas.SetWindowSize(1366, 768)
         style = gROOT.GetStyle("tdrStyle")
+        style.SetCanvasDefH(768) #Height of canvas
+        style.SetCanvasDefW(1366) #Width of canvas
         style.SetOptTitle(1)
         style.SetTitleBorderSize(0)
         style.SetTitleFontSize(0.08)
@@ -85,7 +92,8 @@ class corrections_plotter:
         style.SetPadLeftMargin(0.15)
         style.SetPadRightMargin(0.02)
         style.cd()
-        
+
+    # Read in a data file or sign convention file
     def readFile(self, f_name, sign_conv = False):
         file_ = open(f_name, 'r')
 
@@ -130,7 +138,7 @@ class corrections_plotter:
         else:
             self.data.append(file_cols)
 
-    #identifies the type of the data
+    # Identifies the type of the data
     def getType(self, s):
         if self.FLOAT_RE.match(s):
             value_type = 'float'
@@ -140,7 +148,7 @@ class corrections_plotter:
             value_type = ''
         return value_type
 
-    #applies sign conventions
+    # Applies sign conventions to the data (overwrites data, should only be done once)
     def applySC(self):
         for f_n in range(0,len(self.data)):
             for i in self.scm:
@@ -149,7 +157,7 @@ class corrections_plotter:
                 elif(len(i) == 3):
                     self.data[f_n][i[0]] = [a*b*c for a, b, c in zip(self.data[f_n][i[0]], self.sign_conventions[i[1]], self.sign_conventions[i[2]])]
 
-    #formats data for use in TGraphErrors and passes to plotGrid
+    # Formats data for use in TGraphErrors and passes to plotGrid
     def plot(self, f, x_var, y_var, xo_var, yo_var):
         self.x_var = x_var
         self.y_var = y_var
@@ -157,9 +165,9 @@ class corrections_plotter:
         self.yo_var = yo_var
         self.file_list = f
         
-        var = [x_var, y_var, 'e'+x_var, 'e'+y_var, xo_var, yo_var]
-        include = [1, 1, 1, 1, 1, 1]
-        is_include = [0, 0, 0, 0, 1, 1]
+        var = [x_var, y_var, 'e'+x_var, 'e'+y_var, xo_var, yo_var] # name list of variables used in plotting
+        include = [1, 1, 1, 1, 1, 1] # specify whether to include data or fill with zeroes
+        is_include = [0, 0, 0, 0, 1, 1] # specify whether it is necessary to use an index shift
         for f_n in f:
             for i in range(0,len(var)):
                 if (not var[i] in self.data[f_n].keys()):
@@ -239,7 +247,7 @@ class corrections_plotter:
         
         self.plotGrid(bounds, d)
 
-    #generates a grid of plots
+    # Generates a grid of plots from passed data and set parameters
     def plotGrid(self, bounds, d):
 
         w, h = self.n_items[self.xo_var], self.n_items[self.yo_var]
@@ -247,6 +255,7 @@ class corrections_plotter:
         x_min, y_min, x_max, y_max = bounds
         
         if(self.all_plots):
+            # Reset plot_range to include all possible plots in grid
             self.plot_range = (
             0-self.index_shift[self.xo_var],
             0-self.index_shift[self.yo_var],
@@ -254,6 +263,7 @@ class corrections_plotter:
             (h-1)-self.index_shift[self.yo_var])
             rx_max, ry_max, rx_min, ry_min = (w,h,0,0)
         else:
+            # Get mins and maxs from plot_range, apply index correction, and ensure that they are in range of acceptable values
             rx_min, ry_min, rx_max, ry_max = self.plot_range
             rx_min = min(max(rx_min + self.index_shift[self.xo_var], 0), w)
             ry_min = min(max(ry_min + self.index_shift[self.yo_var], 0), h)
@@ -261,25 +271,24 @@ class corrections_plotter:
             ry_max = max(min(ry_max + self.index_shift[self.yo_var] + 1, h), 0)
             
         self.canvas.Divide(rx_max-rx_min,ry_max-ry_min)
-
-        print self.file_list
         
-        self.hps = []
-        self.frames = []
+        self.hps = [] # TGraphErrors objects arranged by pad location
+        self.frames = [] # TH1F objects arranged by pad location
         for f_n in range(0, len(self.data)):
             self.hps.append([])
-            for i in range(rx_min, rx_max):
+            for i in range(0, rx_max-rx_min):
                 self.hps[f_n].append([])
-        for i in range(rx_min, rx_max):
+        for i in range(0, rx_max-rx_min):
+            ii = i + rx_min
             self.frames.append([])
-            self.framess.append([])
-            for j in range(ry_min, ry_max):
+            for j in range(0, ry_max-ry_min):
+                jj = j + ry_min
                 self.canvas.cd(i + j*(rx_max-rx_min) + 1)
                 self.frames[i].append(self.canvas.DrawFrame(x_min, y_min, x_max, y_max))
                 self.frames[i][j].SetTitle("%s:%d, %s:%d" % (self.xo_var,
-                                                                   i - self.index_shift[self.xo_var],
+                                                                   ii - self.index_shift[self.xo_var],
                                                                    self.yo_var,
-                                                                   j - self.index_shift[self.yo_var]))
+                                                                   jj - self.index_shift[self.yo_var]))
                 self.frames[i][j].GetXaxis().SetTitle(self.x_var)
                 self.frames[i][j].GetYaxis().SetTitle(self.y_var)
                 self.frames[i][j].GetXaxis().SetTitleSize(.07)
@@ -287,13 +296,14 @@ class corrections_plotter:
                 self.frames[i][j].GetXaxis().CenterTitle(True)
                 self.frames[i][j].GetYaxis().CenterTitle(True)
                 for f_n in self.file_list:
-                    n = min(len(d[f_n][0][i][j]), len(d[f_n][1][i][j]), len(d[f_n][2][i][j]), len(d[f_n][3][i][j]))
-                    self.hps[f_n][i].append(TGraphErrors(n, array.array("d", d[f_n][0][i][j]),
-                                                         array.array("d", d[f_n][1][i][j]),
-                                                         array.array("d", d[f_n][2][i][j]),
-                                                         array.array("d", d[f_n][3][i][j])))
+                    n = min(len(d[f_n][0][ii][jj]), len(d[f_n][1][ii][jj]), len(d[f_n][2][ii][jj]), len(d[f_n][3][ii][jj]))
+                    self.hps[f_n][i].append(TGraphErrors(n, array.array("d", d[f_n][0][ii][jj]),
+                                                         array.array("d", d[f_n][1][ii][jj]),
+                                                         array.array("d", d[f_n][2][ii][jj]),
+                                                         array.array("d", d[f_n][3][ii][jj])))
                     self.hps[f_n][i][j].SetMarkerColor(self.f_colors[f_n])
-                    self.hps[f_n][i][j].SetMarkerStyle(7)
+                    self.hps[f_n][i][j].SetMarkerStyle(21)
+                    self.hps[f_n][i][j].SetMarkerSize(.4)
                     self.hps[f_n][i][j].SetFillStyle(0)
                     self.hps[f_n][i][j].SetMaximum(y_max)
                     self.hps[f_n][i][j].SetMinimum(y_min)
@@ -302,35 +312,110 @@ class corrections_plotter:
                 #self.canvas.cd(i + j*(rx_max-rx_min) + 1)
                 self.padRefresh()
 
+    # Refresh the pad after updating
     def padRefresh(self):
         self.canvas.Modified()
         self.canvas.GetFrame().SetBorderSize(12);
         self.canvas.Update()
-       
-    def tca(self, array):
-        n = len(array)
-        if (not n == 0):
-            t = type(array[0])
-            if (t == int):
-                ct = ctypes.c_int
-            elif (t == float):
-                ct = ctypes.c_float
-            elif (t == long):
-                ct = ctypes.c_long
-        C_ARRAY = ct * n
-        c_array = C_ARRAY()
 
-        i = 0
-        for c in c_array:
-            c = ct(array[i])
-            i+=1
-        return c_array
+# A container for corrections_plotter that provides some useful functionality
+class plotContainer:
+    def __init__(self):
+        self.ar = []
+        self.ar_names = []
+        self.x_var = ''
+        self.y_var = ''
+        self.xo_var = ''
+        self.yo_var = ''
+        self.plotter = corrections_plotter()
+    def readFile(self, s, b = False):
+        self.plotter.readFile(s,b)
+    def setPlotRange(self, x_min, y_min, x_max, y_max):
+        self.plotter.setPlotRange(x_min, y_min, x_max, y_max)
+    def setPlotAll(self):
+        self.plotter.setPlotAll()
+    def applySC(self):
+        self.plotter.applySC()
+    # Select a specific plot to generate
+    def select(self, x, y):
+        self.plotter.setPlotRange(x, y, x, y)
+    def plot(self):
+        self.plotter.createCanvas()
+        self.plotter.plot(self.ar, self.x_var, self.y_var, self.xo_var, self.yo_var)
+    # Save a plot as a png with a nicely formatted name
+    def save(self):
+        x_min, y_min, x_max, y_max = self.plotter.plot_range
+        s = "%s-%s-%s" % (self.x_var, self.y_var, self.xo_var)
+        if x_min == x_max:
+            s += "[%d]" % x_min
+        else:
+            s += "[%d,%d]" % (x_min, x_max)
+        s += "-%s" % self.yo_var
+        if y_min == y_max:
+            s += "[%d] " % y_min
+        else:
+            s += "[%d,%d] " % (y_min, y_max)
+        sep = "__"
+        for i in self.ar:
+            s += self.ar_names[i] + sep
+        s = s[:len(sep)*-1]
+        s += ".png"
+        self.plotter.canvas.SaveAs(s)
+        print s
+    # Plot and save
+    def plotSave(self):
+        self.plot()
+        self.save()
+    # Plot and save all possible plots in the grid as individual plots
+    def plotSaveAll(self):
+        w = self.plotter.n_items[self.xo_var]
+        ws = self.plotter.index_shift[self.xo_var]
+        h = self.plotter.n_items[self.yo_var]
+        hs = self.plotter.index_shift[self.yo_var]
+        for i in range(0,w):
+            ii = i - ws
+            for j in range(0,h):
+                jj = j - hs
+                print (ii, jj)
+                self.plotter.createCanvas()
+                self.plotter.canvas.SetWindowSize(850, 700)
+                self.select(ii, jj)
+                self.plotter.plot(self.ar, self.x_var, self.y_var, self.xo_var, self.yo_var)
+                self.save()
 
-def printCArray(carray):
-    s = "["
-    for i in carray:
-        s += str(i)
-        s += ","
-    s = s[:-1]
-    s += "]"
-    print s
+def main(pc): 
+    pc.x_var = 'sector'
+    pc.yo_var = 'station'
+    pc.xo_var = 'wheel'
+
+    os.chdir("../text")
+
+    pc.readFile('res_st1234_dumpfidgttn.txt')
+    pc.readFile('res_st1234_dumpfidgttn_c1.txt')
+    pc.readFile('res_st1234_data_1110_110011_dumplucaini_01.txt')
+    pc.ar_names = ["oldIter1","oldIter2","newIter1"]
+    pc.readFile('sign_conventions_mb.txt', True)
+    pc.applySC()
+
+    os.chdir("..\plots")
+
+    pc.ar = [0,2]
+
+    yvars = ['phiz_100011','z_101000']
+
+    for y in yvars:
+        pc.y_var = y
+
+        pc.setPlotAll()
+        pc.plotSave()
+        
+        pc.setPlotRange(-2,1,2,1)
+        pc.plotSave()
+
+        pc.setPlotRange(0,1,0,4)
+        pc.plotSave()
+        
+        pc.plotSaveAll()
+
+pc = plotContainer()
+main(pc)
