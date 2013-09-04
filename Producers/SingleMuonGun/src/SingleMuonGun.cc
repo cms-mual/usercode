7c3e358c06b78d0f82e304d20e3a33b8da1c9a61
pakhotin <>
2013-09-04 18:37:43 +0000

    initial submission

---
+++ Producers/SingleMuonGun/src/SingleMuonGun.cc
@@ -0,0 +1,18 @@
+// -*- C++ -*-
+//
+// Package:    SingleMuonGun
+// Class:      SingleMuonGun
+// 
+/**\class SingleMuonGun SingleMuonGun.cc SingleMuonGun/src/SingleMuonGun.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Yuriy Pakhotin,,,
+//         Created:  Wed Sep  4 13:36:33 CDT 2013
+// $Id:  $
+//
+//
